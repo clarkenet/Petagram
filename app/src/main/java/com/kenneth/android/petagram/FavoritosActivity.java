7032b47f969f7e14570fb41d1c3cbcf85a1c9901
Kenneth Castillo <pactoreal@msn.com>
2016-08-03 13:03:40 -0500

    Se agrega Action View para ir a la Lista de Mascotas Favoritas

--- app/src/main/java/com/kenneth/android/petagram/FavoritosActivity.java
+++ app/src/main/java/com/kenneth/android/petagram/FavoritosActivity.java
@@ -1,34 +1,30 @@
 package com.kenneth.android.petagram;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.kenneth.android.petagram.com.kenneth.android.petagram.adapter.MascotaAdapter;
 import com.kenneth.android.petagram.com.kenneth.android.petagram.model.Mascota;
 
 import java.util.ArrayList;
 
-public class ListadoActivity extends AppCompatActivity {
+public class FavoritosActivity extends AppCompatActivity {
 
     ArrayList<Mascota> mascotas;
-    RecyclerView rvMascotas;
+    RecyclerView rvMascotasFav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_listado);
-//        getActionBar().setDisplayHomeAsUpEnabled(true);
+        setContentView(R.layout.activity_favoritos);
 
-        rvMascotas = (RecyclerView) findViewById(R.id.rvMascotas);
+        rvMascotasFav = (RecyclerView) findViewById(R.id.rvMascotasFav);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
 
-        rvMascotas.setLayoutManager(linearLayoutManager);
+        rvMascotasFav.setLayoutManager(linearLayoutManager);
 
         inicializarListaMascotas();
         inicializarAdaptador();
@@ -38,39 +34,15 @@ public class ListadoActivity extends AppCompatActivity {
     private void inicializarListaMascotas() {
         mascotas = new ArrayList<>();
         mascotas.add(new Mascota(R.drawable.puppy_black_brown, "Puppy"));
-        mascotas.add(new Mascota(R.drawable.puppy_black_white, "Perry"));
-        mascotas.add(new Mascota(R.drawable.puppy_brown, "Aslan"));
         mascotas.add(new Mascota(R.drawable.puppy_bulldog, "Pet"));
         mascotas.add(new Mascota(R.drawable.puppy_husky, "Balto"));
-        mascotas.add(new Mascota(R.drawable.puppy_labrador, "Lambda"));
-        mascotas.add(new Mascota(R.drawable.puppy_pitbull, "Daddy"));
         mascotas.add(new Mascota(R.drawable.puppy_white, "Snow"));
         mascotas.add(new Mascota(R.drawable.puppy_wolf, "Lobo"));
     }
 
     private void inicializarAdaptador() {
         MascotaAdapter adapter = new MascotaAdapter(mascotas);
-        rvMascotas.setAdapter(adapter);
+        rvMascotasFav.setAdapter(adapter);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_opciones_listado, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        switch (item.getItemId()) {
-
-            case R.id.action_favoritos:
-                Intent intent = new Intent(this, FavoritosActivity.class);
-                startActivity(intent);
-                break;
-
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
